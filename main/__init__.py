656b5d38e6a73b67b6610995834169332c24b3cf
Dereje A Teshager <derejeteshager0@gmail.com>
2025-06-10 21:26:37 -0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -5,9 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.contrib.github import make_github_blueprint, github
-
 from dotenv import load_dotenv
 
 load_dotenv()
